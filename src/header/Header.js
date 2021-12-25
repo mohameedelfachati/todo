5b3e8e7435593bed7352b8f6384d51dd8319c692
mohameedelfachati <mohameedelfachati@gmail.com>
2021-12-25 16:39:08 +0100

    fix logo header

--- src/header/Header.js
+++ src/header/Header.js
@@ -1,12 +1,12 @@
 import React from "react";
 import "./header.css";
-import icon from "../img/nmbleways-favicon-1.png";
+import icon from "../img/skull-logo.png";
 import Deconnection from "../svg/Deconnection/DeconnectionIcon";
 
 const Header = () => {
   return (
     <div className="header">
-      <img src="" alt="nfjzenf" className="icon" />
+      <img src={icon} alt="nfjzenf" className="icon" />
       <button className="button_container">
         <Deconnection />
         <span className="text_deconnection">Déconnection</span>
